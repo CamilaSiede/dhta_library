b432864f7f844d9d16063509b580ef74d60ce5ba
CamilaSiede <86715987+CamilaSiede@users.noreply.github.com>
2022-03-07 15:55:35 +0100

    Update settings.py

--- DHTA_library/settings.py
+++ DHTA_library/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'welcome',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'DHTA_library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.DB_NAME,
+        'USER': config.DB_USER,
+        'PASSWORD': config.DB_PASSWORD
     }
 }
 
